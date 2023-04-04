8a266ba3a2734b8bff1a372c0d59f017c05d2033
FranciscoAscue <francisco.ascue131@gmail.com>
2023-04-03 23:35:56 -0500

    update

--- dir_files.R
+++ dir_files.R
@@ -1,22 +1,22 @@
-dir.create(paste0(getwd(), "data"))
-           dir.create(paste0(getwd(), "data/reference"))
+dir.create(paste0(getwd(), "/data"))
+           dir.create(paste0(getwd(), "/data/reference"))
                       url_download <- "https://raw.githubusercontent.com/FranciscoAscue/Rgenomics/master/data/reference/ref.json"
-                      download.file(url_download,"data/reference/ref.json")
-           dir.create(paste0(getwd(), "data/rawdata"))
-           dir.create(paste0(getwd(), "data/processed_data"))
-dir.create(paste0(getwd(), "scripts"))
-dir.create(paste0(getwd(), "results"))
+                      download.file(url_download,"/data/reference/ref.json")
+           dir.create(paste0(getwd(), "/data/rawdata"))
+           dir.create(paste0(getwd(), "/data/processed_data"))
+dir.create(paste0(getwd(), "/scripts"))
+dir.create(paste0(getwd(), "/results"))
                       
 url_download <- "https://raw.githubusercontent.com/FranciscoAscue/Rgenomics/master/install.R"
-download.file(url_download,paste0(getwd(),"install.R"))
+download.file(url_download,paste0(getwd(),"/install.R"))
 url_download <- "https://raw.githubusercontent.com/FranciscoAscue/Rgenomics/master/main.R"
-download.file(url_download,paste0(getwd(),"main.R"))
+download.file(url_download,paste0(getwd(),"/main.R"))
 
 url_download <- "https://raw.githubusercontent.com/FranciscoAscue/Rgenomics/master/scripts/sratoolkit.R"
-download.file(url_download,paste0(getwd(),"scripts/sratoolkit.R"))
+download.file(url_download,paste0(getwd(),"/scripts/sratoolkit.R"))
 url_download <- "https://raw.githubusercontent.com/FranciscoAscue/Rgenomics/master/scripts/listfastq.R"
-download.file(url_download,paste0(getwd(),"scripts/listfastq.R"))
+download.file(url_download,paste0(getwd(),"/scripts/listfastq.R"))
 url_download <- "https://raw.githubusercontent.com/FranciscoAscue/Rgenomics/master/scripts/filtereads.R"
-download.file(url_download,paste0(getwd(),"scripts/filtereads.R"))
+download.file(url_download,paste0(getwd(),"/scripts/filtereads.R"))
 url_download <- "https://raw.githubusercontent.com/FranciscoAscue/Rgenomics/master/scripts/consensus.R"
-download.file(url_download,paste0(getwd(),"scripts/consensus.R"))
+download.file(url_download,paste0(getwd(),"/scripts/consensus.R"))
