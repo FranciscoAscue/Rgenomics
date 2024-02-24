ab3563929f0f4cb3d1d4d03ae1d0ed2e031ddd60
Francisco Ascue Orosco <57104899+FranciscoAscue@users.noreply.github.com>
2024-02-24 12:36:10 -0500

    Update linux_libs_R.bash

--- linux_libs_R.bash
+++ linux_libs_R.bash
@@ -3,3 +3,4 @@ sudo apt install libxml2-dev ## theme impor Rstudio
 sudo apt install libglpk-dev libxml2-dev ## adegenet error install igraph fatal error: glpk.h: No such file or directory
 
 sudo apt install default-jdk ## rjava package
+sudo apt install libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev ### devtools packages
