9cfeca4f9e5c30a041e764a127f4c0d66a4c1e41
Francisco Ascue Orosco <57104899+FranciscoAscue@users.noreply.github.com>
2024-03-15 01:47:00 -0500

    Update linux_libs_R.bash

--- linux_libs_R.bash
+++ linux_libs_R.bash
@@ -1,5 +1,5 @@
 sudo apt install libcurl4-openssl-dev ## Install KEGGREST Package
-sudo apt install libxml2-dev ## theme impor Rstudio
+sudo apt install libxml2-dev ## theme import Rstudio
 sudo apt install libglpk-dev libxml2-dev ## adegenet error install igraph fatal error: glpk.h: No such file or directory
 
 sudo apt install default-jdk ## rjava package
